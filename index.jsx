c13b497ff886b8228f3682626b18a5694f14670f
ganbud_munkhtulga88 <76542111+Kclaus88@users.noreply.github.com>
2021-05-10 10:13:06 +0800

    instagram-2 post
    run index.jsx

---
+++ index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/header';
+import Post from '../components/post'
+
+function App() {
+    return (
+        <>
+            <Header/>
+            <Post/>
+        </>
+    )
+}
+
+export default App;
