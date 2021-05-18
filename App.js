c5dab86ed49d1317af16924c14c7c06541554f4b
ganbud_munkhtulga88 <76542111+Kclaus88@users.noreply.github.com>
2021-05-18 12:44:23 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './components/index';
+import Account from './components/account'
+function App() {
+  return (
+    <div>
+      <Home />
+      <Account />
+    </div>
+  );
+}
+
+export default App;
